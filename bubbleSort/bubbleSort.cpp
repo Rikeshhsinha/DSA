312a5190ea008964d33d8ceb9e3bb13f73f83127
Rikesh Kumar <rikeshsinha245@gmail.com>
2026-09-18 16:10:12 +0530

    Add bubble sort algo

---
+++ bubbleSort/bubbleSort.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void bubbleSort(vector<int> &num, int n)
+{
+
+    for(int i = n-1; i > 0 ; i--){
+
+        for( int j = 0 ; j < i ; j++ ){
+            if(num[j] > num [ j+1] ){
+                swap(num[j],num[j+1]);
+            }
+        }
+    }
+
+   
+}
+
+int main()
+{
+
+    int n;
+    vector<int> num;
+
+    cout << "Enter how many element want to store :";
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+
+        cout << "Enter the value of index " << i << ":";
+        int x;
+        cin >> x;
+        num.push_back(x);
+    }
+
+    bubbleSort(num, n);
+
+    cout << "After sorting the array is :";
+
+    for (int i = 0; i < n; i++)
+    {
+
+        cout << num[i]<<" ";
+    }
+
+    return 0;
+}
