37ccd06f932bd08b2e86819363952a336f56aa69
Rikesh Kumar <rikeshsinha245@gmail.com>
2026-09-18 16:25:58 +0530

    Add optimize the bubble sort algo

--- bubbleSort/bubbleSort.cpp
+++ bubbleSort/bubbleSort.cpp
@@ -6,16 +6,25 @@ using namespace std;
 void bubbleSort(vector<int> &num, int n)
 {
 
-    for(int i = n-1; i > 0 ; i--){
+    for (int i = n - 1; i > 0; i--)
+    {
 
-        for( int j = 0 ; j < i ; j++ ){
-            if(num[j] > num [ j+1] ){
-                swap(num[j],num[j+1]);
+        int didSwap = 0;
+
+        for (int j = 0; j < i; j++)
+        {
+            if (num[j] > num[j + 1])
+            {
+                swap(num[j], num[j + 1]);
+
+                didSwap = 1;
             }
         }
+        if (didSwap == 0)
+        {
+            break;
+        }
     }
-
-   
 }
 
 int main()
@@ -43,7 +52,7 @@ int main()
     for (int i = 0; i < n; i++)
     {
 
-        cout << num[i]<<" ";
+        cout << num[i] << " ";
     }
 
     return 0;
